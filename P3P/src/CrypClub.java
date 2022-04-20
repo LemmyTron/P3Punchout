527676573f70d1e9292eb2ca35ce4250da12b726
1861949 <1861949@01-19636.pusd.dom>
2022-04-20 13:56:15 -0700

    finished charactrer constructors

---
+++ P3P/src/CrypClub.java
@@ -0,0 +1,7 @@
+
+public class CrypClub extends Character{
+	public CrypClub(int x, int y, int health, String pers) {
+		super(x, y, 130, pers, 6.9);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ P3P/src/MD.java
@@ -0,0 +1,7 @@
+
+public class MD extends Character{
+	public MD(int x, int y, int health, String pers) {
+		super(x, y, 105, pers, 6.98);
+		// TODO Auto-generated constructor stub
+	}
+}
