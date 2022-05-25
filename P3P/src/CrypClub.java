a9213c4802c4c048861eae8e0cf72bc126c49a4e
bellasantos <bellasantos@localhost>
2022-05-25 13:49:12 -0700

    troubleshooting issues in crypclub class

--- P3P/src/CrypClub.java
+++ P3P/src/CrypClub.java
@@ -1,9 +1,9 @@
 
 public class CrypClub extends Character{
-	//public CrypClub(int x, int y, int health, String pers, boolean whichWay) {
-		//super(x, y, 130, pers, 6.9, whichWay, g);
+	public CrypClub(int x, int y, int health, String pers, boolean whichWay) {
+		super(x, y, 130, pers, 6.9, whichWay, 0.35);
 		// TODO Auto-generated constructor stub
-	//}
+	}
 	
 	public void throwIt() {
 		//if the obj intersects w the box
