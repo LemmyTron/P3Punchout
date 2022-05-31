9aaed6f3cb3f7f0d0386e0739e77cc1de0d131fb
bellasantos <bellasantos@localhost>
2022-05-31 09:10:40 -0700

    ?

--- P3P/src/Aak.java
+++ P3P/src/Aak.java
@@ -2,7 +2,7 @@
 public class Aak extends Character{
 	public Aak(int x, int y, boolean whichWay) {
 		super(x, y, 95, "aakarsh.png", "aakarshrev.png", "aakarshpunch.png", "aakarshpunchrev.png", 8, whichWay, 0.35);
-		// TODO Auto-generated constructor stub //
+		// TODO Auto-generated constructor stub 
 	}
 	public int getX() {
 		return getX();
